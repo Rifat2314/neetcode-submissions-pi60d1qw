c99e4dccf7fe9f7a706ae12b72305ab2e24efef5
Muhd Rifat Uddin <110630361+Rifat2314@users.noreply.github.com>
2026-05-02 01:05:18 +0600

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[10001];
+    int solve(vector<int>& coins, int amount){
+        if(amount==0){
+            return 0;
+        }
+        if(dp[amount]!=-1)return dp[amount];
+        int res=1e9;
+        for(int i=0;i<coins.size();i++){
+            if(amount-coins[i]>=0){
+                res = min(res,1+solve(coins,amount-coins[i]));
+            }
+        }
+        return dp[amount] = res;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+       for(int i=0;i<=10000;i++){
+            dp[i]=-1;
+       } 
+       int ans = solve(coins,amount); 
+       return (ans >= 1e9) ? -1 : ans;
+    }
+};
